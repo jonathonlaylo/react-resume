a53937734e90b7e1f80a3e7c13caac5b41518fd3
jonathonlaylo <jonathon.laylo@gmail.com>
2018-04-01 16:16:42 -0700

    added right container content

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -3,12 +3,14 @@ import './App.css';
 
 //Components
 import Left from "../Left";
+import Right from "../Right";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Left />
+        <Right />
       </div>
     );
   }
